a0890e1e0d7434a9c6c138337a5756ababeb83bb
Morgan Hart <morganhart@Morgans-MacBook-Pro.local>
2014-12-07 22:27:15 -0500

    First commit

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cash = 10
+    var lemons = 1
+    var iceCubes = 1
+    
+    var purchasedLemons = 0
+    var purchasedIceCubes = 0
+    
+    var mixedLemons = 0
+    var mixedIceCubes = 0
+    
+    @IBOutlet weak var totalCashLabel: UILabel!
+    @IBOutlet weak var totalLemonsLabel: UILabel!
+    @IBOutlet weak var totalIceCubesLabel: UILabel!
+    
+    @IBOutlet weak var purchasedLemonsLabel: UILabel!
+    @IBOutlet weak var purchasedIceCubesLabel: UILabel!
+    
+    @IBOutlet weak var mixedLemonsLabel: UILabel!
+    @IBOutlet weak var mixedIceCubesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +40,180 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func purchaseLemonAddButtonPressed(sender: UIButton) {
+    
+        if cash <= 1 {
+        
+            println("You do not have enough cash to purchase lemons.")
+        
+        }
+        
+        else {
+            
+            cash -= 2
+            lemons += 1
+            purchasedLemons += 1
+            
+            purchasedLemonsLabel.text = "\(purchasedLemons)"
+            totalCashLabel.text = "$\(cash)"
+            totalLemonsLabel.text = "\(lemons) Lemons"
+        
+        }
+        
+    }
 
+    @IBAction func purchaseLemonSubtractButtonPressed(sender: UIButton) {
+    
+        if purchasedLemons == 0 {
+            
+            println("You have not purchased any lemons.")
+        
+        }
+        
+        else if lemons == mixedLemons {
+            
+            println("You cannot remove any lemons.")
+            
+        }
+        
+        else {
+        
+            cash += 2
+            lemons -= 1
+            purchasedLemons -= 1
+            
+            purchasedLemonsLabel.text = "\(purchasedLemons)"
+            totalCashLabel.text = "$\(cash)"
+            totalLemonsLabel.text = "\(lemons) Lemons"
+        
+        }
+    
+    }
+    
+    @IBAction func purchaseIceCubesAddButtonPressed(sender: UIButton) {
+    
+        if cash == 0 {
+            
+            println("You do not have enough cash to purchase ice cubes.")
+            
+        }
+            
+        else {
+            
+            cash -= 1
+            iceCubes += 1
+            purchasedIceCubes += 1
+            
+            purchasedIceCubesLabel.text = "\(purchasedIceCubes)"
+            totalCashLabel.text = "$\(cash)"
+            totalIceCubesLabel.text = "\(iceCubes) Ice Cubes"
+            
+        }
+        
+    }
+    
+    @IBAction func purchaseIceCubesSubtractButtonPressed(sender: UIButton) {
+        
+        if purchasedIceCubes == 0 {
+            
+            println("You have not purchased any ice cubes.")
+            
+        }
+            
+        else if iceCubes == mixedIceCubes {
+            
+            println("You cannot remove any ice cubes.")
+            
+        }
+        
+        else {
+            
+            cash += 1
+            iceCubes -= 1
+            purchasedIceCubes -= 1
+            
+            purchasedIceCubesLabel.text = "\(purchasedIceCubes)"
+            totalCashLabel.text = "$\(cash)"
+            totalIceCubesLabel.text = "\(iceCubes) Ice Cubes"
+            
+        }
+    
+    }
+    
+    @IBAction func mixLemonsAddButtonPressed(sender: UIButton) {
+        
+        if mixedLemons == lemons {
+            
+            println("You don't have any more lemons to mix.")
+        
+        }
+        
+        else {
+            
+            mixedLemons += 1
+            
+            mixedLemonsLabel.text = "\(mixedLemons)"
+            
+        }
+    
+    }
+    
+    @IBAction func mixLemonsSubtractButtonPressed(sender: UIButton) {
+    
+        if mixedLemons == 0 {
+            
+            println("You don't have any mixing lemons.")
+            
+        }
+        
+        else {
+            
+            mixedLemons -= 1
+            
+            mixedLemonsLabel.text = "\(mixedLemons)"
+            
+        }
+        
+    }
+    
+    @IBAction func mixIceCubesAddButtonPressed(sender: UIButton) {
+    
+        if mixedIceCubes == iceCubes {
+            
+            println("You don't have any more ice cubes to mix.")
+            
+        }
+        
+        else {
+            
+            mixedIceCubes += 1
+            
+            mixedIceCubesLabel.text = "\(mixedIceCubes)"
+            
+        }
+    
+    }
+    
+    @IBAction func mixIceCubesSubtractButtonPressed(sender: UIButton) {
+    
+        if mixedIceCubes == 0 {
+            
+            println("You don't have any mixing ice cubes.")
+            
+        }
+        
+        else {
+            
+            mixedIceCubes -= 1
+            
+            mixedIceCubesLabel.text = "\(mixedIceCubes)"
+        
+        }
+    
+    }
+    
+    @IBAction func startDayButtonPressed(sender: UIButton) {
+    }
+    
 }
 
